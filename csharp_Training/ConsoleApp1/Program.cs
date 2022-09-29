0365b5189432d1975183e5c6459473dfc1baa280
naveena-yenna <111953604+naveena-yenna@users.noreply.github.com>
2022-09-29 12:13:58 +0530

    Add files via upload

---
+++ csharp_Training/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 20.0;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
